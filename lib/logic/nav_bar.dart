40ab91f247dc7f5254f6197b4ca735508f092880
user.email <masa81hiro69@gmail.com>
2023-08-20 19:01:48 +0900

    [add] nav_bar可愛いのあったから、それにしたい

---
+++ lib/logic/nav_bar.dart
@@ -0,0 +1,154 @@
+import 'dart:developer';
+
+import 'package:animated_notch_bottom_bar/animated_notch_bottom_bar/animated_notch_bottom_bar.dart';
+import 'package:flutter/material.dart';
+
+import '../constant.dart';
+
+class NavBar extends StatefulWidget {
+  const NavBar({Key? key}) : super(key: key);
+
+  @override
+  State<NavBar> createState() => _NavBarState();
+}
+
+class _NavBarState extends State<NavBar> {
+  /// Controller to handle PageView and also handles initial page
+  final _pageController = PageController(initialPage: 0);
+
+  /// Controller to handle bottom nav bar and also handles initial page
+  final _controller = NotchBottomBarController(index: 0);
+
+  int maxCount = 3;
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  /// widget list
+  final List<Widget> bottomBarPages = [
+    const Page1(),
+    const Page2(),
+    const Page3(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Smart Handagote',
+          style: TextStyle(color: Constant.lightGray),
+        ),
+        backgroundColor: Constant.darkGray,
+      ),
+      body: PageView(
+        controller: _pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: List.generate(
+            bottomBarPages.length, (index) => bottomBarPages[index]),
+      ),
+      extendBody: true,
+      bottomNavigationBar: (bottomBarPages.length <= maxCount)
+          ? AnimatedNotchBottomBar(
+              /// Provide NotchBottomBarController
+              notchBottomBarController: _controller,
+              color: Constant.darkGray,
+              showLabel: false,
+              notchColor: Constant.darkGray,
+
+              /// restart app if you change removeMargins
+              removeMargins: false,
+              bottomBarWidth: 500,
+              durationInMilliSeconds: 300,
+              bottomBarItems: const [
+                BottomBarItem(
+                  inActiveItem: Icon(
+                    Icons.history,
+                    color: Constant.lightGray,
+                  ),
+                  activeItem: Icon(
+                    Icons.history,
+                    color: Constant.lightGray,
+                  ),
+                  itemLabel: 'Page 1',
+                ),
+                BottomBarItem(
+                  inActiveItem: Icon(
+                    Icons.home_filled,
+                    color: Constant.lightGray,
+                  ),
+                  activeItem: Icon(
+                    Icons.home_filled,
+                    color: Constant.lightGray,
+                  ),
+                  itemLabel: 'Page 2',
+                ),
+
+                BottomBarItem(
+                  inActiveItem: Icon(
+                    Icons.settings,
+                    color: Constant.lightGray,
+                  ),
+                  activeItem: Icon(
+                    Icons.settings,
+                    color: Constant.lightGray,
+                  ),
+                  itemLabel: 'Page 2',
+                ),
+
+                ///svg example
+                // BottomBarItem(
+                //   inActiveItem: SvgPicture.asset(
+                //     'assets/search_icon.svg',
+                //     color: Constant.lightGray,
+                //   ),
+                //   activeItem: SvgPicture.asset(
+                //     'assets/search_icon.svg',
+                //     color: Constant.lightGray,
+                //   ),
+                //   itemLabel: 'Page 3',
+                // ),
+              ],
+              onTap: (index) {
+                /// perform action on tab change and to update pages you can update pages without pages
+                log('current selected index $index');
+                _pageController.jumpToPage(index);
+              },
+            )
+          : null,
+    );
+  }
+}
+
+class Page1 extends StatelessWidget {
+  const Page1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Constant.black, child: const Center(child: Text('Page 1')));
+  }
+}
+
+class Page2 extends StatelessWidget {
+  const Page2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Constant.black, child: const Center(child: Text('Page 2')));
+  }
+}
+
+class Page3 extends StatelessWidget {
+  const Page3({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Constant.black, child: const Center(child: Text('Page 3')));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:smart_handagote/logic/nav_bar.dart';
 import 'package:smart_handagote/view/test_reservation_page.dart';
 import 'package:smart_handagote/view/test_update_role_page.dart';
 
@@ -31,55 +32,8 @@ class MyApp extends StatelessWidget {
       // home: const TestLoginPage(),
       // home: const UserManagementPage(),
       // home: const MyHomePage(),
-      home: const TestReservationPage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // home: const TestReservationPage(),
+      home: const NavBar(),
     );
   }
 }
