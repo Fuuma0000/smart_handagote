d854213da3aa0f37b7ede75783decbe6d7bfdae8
user.email <masa81hiro69@gmail.com>
2023-08-20 19:01:08 +0900

    [add] constant

---
+++ lib/constant.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class Constant {
+  static Color darkGray = const Color(0xFF575551).withAlpha(234);
+  // static const Color darkGray = Color(0xFF575551);
+  // static const Color black = Color(0xFF343434);
+  static Color black = const Color(0xFF1A1A1A).withAlpha(240);
+  static const Color lightGray = Color(0xFFDEDBDB);
+}
