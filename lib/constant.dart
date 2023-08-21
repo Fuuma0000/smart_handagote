8675707bd4ab4060cf1e9e913c5fd9f290994d10
user.email <masa81hiro69@gmail.com>
2023-08-21 13:19:12 +0900

    [add] add green

--- lib/constant.dart
+++ lib/constant.dart
@@ -7,4 +7,5 @@ class Constant {
   static Color black = const Color(0xFF1A1A1A).withAlpha(240);
   static const Color lightGray = Color(0xFFDEDBDB);
   static const Color white = Color(0xFF616161);
+  static const Color green = Color(0xFF268785);
 }
