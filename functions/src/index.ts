a280ae575672a2eb51c0e2a5ab3d7d7cead8d211
shiorioz <ozws011.tmt@gmail.com>
2023-08-21 12:00:14 +0900

    [update]startUsingを変更

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -20,10 +20,13 @@ export const startUsing = functions.https.onRequest(async (req, res) => {
   functions.logger.info('startUsing');
 
   try {
-    // usersコレクションの認証
     const { user_id, device_id } = req.body;
-    const userDoc = await admin.firestore().collection('users').doc(user_id).get();
+    const reservationCollection = await admin.firestore().collection('reservations');
+    const logsCollection = await admin.firestore().collection('logs');
+    let log_id;
 
+    // usersコレクションの認証
+    const userDoc = await admin.firestore().collection('users').doc(user_id).get();
     if (!userDoc.exists) {
       res.status(404).send('User not found');
       return;
@@ -35,38 +38,37 @@ export const startUsing = functions.https.onRequest(async (req, res) => {
       return;
     }
 
-    // TODO: テスト
-    // reservationコレクションの確認
-    const reservationCollection = await admin.firestore().collection('reservations');
-    const reservationDocs = await reservationCollection.get();
-    if (!reservationDocs.empty) {
-      res.status(403).send('Device is not available');
-      return;
+    // logsに追加されているか確認（自分の予約の順番が来ている場合）
+    const myLogDoc = await logsCollection.where('user_id', '==', user_id).where('start_time', '==', null).get();
+    if (!myLogDoc.empty) {
+      const logDocRef = logsCollection.doc(myLogDoc.docs[0].id);
+      await logDocRef.update({ start_time: Timestamp.now() });
+      log_id = myLogDoc.docs[0].id;
+    } else {
+      // reservationsコレクションのチェック（他の人の予約が来ている場合）
+      const reservationDocs = await reservationCollection.get();
+      if (!reservationDocs.empty) {
+        res.status(403).send('reservation exists');
+        return;
+      }
+
+      // logsコレクションに追加
+      const logData = {
+        user_id: user_id,
+        device_id: device_id,
+        start_time: Timestamp.now(),
+        end_time: null,
+        is_turn_off: false,
+      };
+      const logRef = await logsCollection.add(logData);
+      log_id = logRef.id;
     }
 
-    // TODO: テスト
-    // reservationコレクションに自分の予約があれば削除
-    const myReservationDoc = await reservationCollection.where('user_id', '==', user_id).get();
-    if (!myReservationDoc.empty) {
-      await myReservationDoc.docs[0].ref.delete();
-    }
-
-    // logsに追加
-    const logData = {
-      user_id: user_id,
-      device_id: device_id,
-      start_time: Timestamp.now(),
-      is_turn_off: false,
-    };
-
-    const logRef = await admin.firestore().collection('logs').add(logData);
-
-    res.status(200).json({ log_id: logRef.id });
+    res.status(200).json({ log_id: log_id });
   } catch (error) {
     functions.logger.error('Error', error);
     res.status(500).send('Internal Server Error');
   }
-
   return;
 });
 
