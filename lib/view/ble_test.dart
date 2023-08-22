80dfa1fe5eaa3c9136ff6322e8c3d893f7aa020d
user.email <masa81hiro69@gmail.com>
2023-08-22 17:45:31 +0900

    [add] install flutter_blue_plus

---
+++ lib/view/ble_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BleTest extends StatefulWidget {
+  const BleTest({super.key});
+
+  @override
+  State<BleTest> createState() => _BleTestState();
+}
+
+class _BleTestState extends State<BleTest> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import cloud_firestore
 import firebase_auth
 import firebase_core
 import firebase_messaging
+import flutter_blue_plus
 import flutter_local_notifications
 import shared_preferences_foundation
 
@@ -17,6 +18,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
