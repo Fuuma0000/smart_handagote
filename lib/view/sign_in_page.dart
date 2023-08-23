402970b427c800e34db2d71c1c8d1f4f141a1bae
shiorioz <ozws011.tmt@gmail.com>
2023-08-23 10:33:59 +0900

    [fix]ログインの不具合を修正した

--- lib/view/sign_in_page.dart
+++ lib/view/sign_in_page.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:smart_handagote/view/sign_up_page.dart';
 
 import '../constant.dart';
+import 'components/dialog.dart';
 
 class SignInPage extends StatefulWidget {
   const SignInPage({super.key});
@@ -12,9 +14,41 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
+  String _email = '';
+  String _password = '';
+  bool _isLoadingLoggingIn = false; // 処理中かどうかを管理するフラグ
+  bool _showPassword = false;
   TextEditingController _emailController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
-  bool _showPassword = false;
+
+  // ログインの処理
+  Future<void> loginUser() async {
+    setState(() {
+      _isLoadingLoggingIn = true;
+    });
+    try {
+      // ログイン
+      final User? user = (await FirebaseAuth.instance
+              .signInWithEmailAndPassword(email: _email, password: _password))
+          .user;
+      // ログインに成功したらダイアログを表示
+      if (user != null) {
+        if (!mounted) return;
+
+        DialogHelper.showCustomDialog(
+            context: context, title: 'ログインしました', message: '');
+      }
+    } catch (e) {
+      if (!mounted) return;
+      DialogHelper.showCustomDialog(
+          context: context, title: 'エラー', message: '');
+      print(e);
+    } finally {
+      setState(() {
+        _isLoadingLoggingIn = false; // 処理完了後に処理中フラグをfalseにセット
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -57,7 +91,14 @@ class _SignInPageState extends State<SignInPage> {
                     SizedBox(
                       width: MediaQuery.of(context).size.width * 0.8,
                       child: ElevatedButton(
-                        onPressed: () {},
+                        onPressed: () async {
+                          _email = _emailController.text;
+                          _password = _passwordController.text;
+
+                          if (!_isLoadingLoggingIn) {
+                            await loginUser();
+                          }
+                        },
                         style: ElevatedButton.styleFrom(
                           backgroundColor: Constant.green,
                           shape: RoundedRectangleBorder(
@@ -65,8 +106,12 @@ class _SignInPageState extends State<SignInPage> {
                           ),
                           padding: const EdgeInsets.symmetric(vertical: 15),
                         ),
-                        child: const Text('登録',
-                            style: TextStyle(color: Colors.white)),
+                        child: _isLoadingLoggingIn // 処理中フラグに基づいてボタンの表示を切り替え
+                            ? const CircularProgressIndicator() // グルグル回るアニメーション
+                            : const Text('ログイン',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18)), // 処理中の場合はボタンを無効に
                       ),
                     ),
                     const SizedBox(height: 20),
