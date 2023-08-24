ab4364bfcd7352f9607f36b4b9e63330f9b7b4f2
shiorioz <ozws011.tmt@gmail.com>
2023-08-25 00:21:01 +0900

    [add]設定画面のウィジェット追加した

--- lib/constant.dart
+++ lib/constant.dart
@@ -7,7 +7,9 @@ class Constant {
   static Color black = const Color(0xFF1A1A1A).withAlpha(240);
   static const Color white = Color(0xFFDEDBDB);
   static const Color lightGrey = Color(0xFF616161);
+  static Color grey = const Color(0xffb5b5b5).withAlpha(240);
   static const Color green = Color(0xFF268785);
   static const Color orange = Color(0xFFD17C3E);
   static const Color yellow = Color(0xFFF2C162);
+  static const Color pink = Color(0xFFB93757);
 }
--- lib/view/setting_page.dart
+++ lib/view/setting_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:smart_handagote/constant.dart';
 
 class SettingPage extends StatefulWidget {
   const SettingPage({super.key});
@@ -11,7 +13,111 @@ class _SettingPageState extends State<SettingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('設定画面')),
+      backgroundColor: Constant.black,
+      body: Column(
+        children: [
+          const SizedBox(height: 40),
+          _userSettingBtnWidget(),
+          _settingBtnWidget(
+            '指紋',
+            FontAwesomeIcons.fingerprint,
+            Constant.grey,
+            () {
+              // TODO: 指紋追加
+            },
+          ),
+          _settingBtnWidget(
+            'はんだごて追加',
+            FontAwesomeIcons.circlePlus,
+            Constant.grey,
+            () {
+              // TODO: はんだごて追加
+            },
+          ),
+          _settingBtnWidget(
+            'ユーザー承認',
+            FontAwesomeIcons.check,
+            Constant.grey,
+            () {
+              // TODO: ユーザー承認
+            },
+          ),
+          _settingBtnWidget(
+              'ログアウト', FontAwesomeIcons.arrowRightFromBracket, Constant.pink,
+              () {
+            // TODO: ログアウト
+          }),
+        ],
+      ),
+    );
+  }
+
+  Widget _userSettingBtnWidget() {
+    return InkWell(
+      onTap: () {
+        // TODO: ユーザー設定
+      },
+      highlightColor: Colors.transparent,
+      splashColor: Colors.transparent,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Constant.grey,
+          borderRadius: const BorderRadius.all(Radius.circular(14)),
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 24),
+        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: const Row(
+          children: [
+            Icon(FontAwesomeIcons.solidUser),
+            Spacer(),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('ユーザー設定',
+                    style:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.w500)),
+                SizedBox(height: 4),
+                Text('名前・メールアドレス・パスワード・学籍番号', style: TextStyle(fontSize: 12)),
+              ],
+            ),
+            Spacer(),
+            Icon(FontAwesomeIcons.angleRight),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _settingBtnWidget(String title, IconData icon, Color backgroundColor,
+      GestureTapCallback onPressed) {
+    Color textColor =
+        (backgroundColor == Constant.pink) ? Constant.white : Constant.black;
+
+    return InkWell(
+      highlightColor: Colors.transparent,
+      splashColor: Colors.transparent,
+      onTap: onPressed,
+      child: Container(
+        decoration: BoxDecoration(
+          color: backgroundColor,
+          borderRadius: const BorderRadius.all(Radius.circular(14)),
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 18),
+        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: Row(
+          children: [
+            Icon(icon),
+            const Spacer(flex: 1),
+            Text(title,
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    color: textColor)),
+            const Spacer(flex: 4),
+            const Icon(FontAwesomeIcons.angleRight),
+          ],
+        ),
+      ),
     );
   }
 }
