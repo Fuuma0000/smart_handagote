02b1ed377ecdc2e40fe06097aefbd9499c9ce922
user.email <masa81hiro69@gmail.com>
2023-08-24 12:06:28 +0900

    [fix] UUIDを-が4つになるように

--- lib/bluetooth_constants.dart
+++ lib/bluetooth_constants.dart
@@ -1,7 +1,7 @@
 class BluetoothConstants {
   const BluetoothConstants._();
 
-  static const serviceUuid = 'F8tskdHqB19OUUA8cgVI';
+  static const serviceUuid = '7c68efdd-a727-4b51-ba18-a3519164875c';
   // FlutterからM5stackにUIDとユーザ名を送信するためのキャラクタリスティックのUUID
   static const sendUidAndNameUuid = '83b35c6d-56ce-4e0b-ad0e-3ced0d1cee75';
 }
