30eda84dd9cbbc4d2fa42b7570988f0e8a1324e5
user.email <masa81hiro69@gmail.com>
2023-08-26 07:22:04 +0900

    [add] 機能を追加したけど、デザインはまだ

--- lib/view/add_fingerprint_page.dart
+++ lib/view/add_fingerprint_page.dart
@@ -1,5 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:smart_handagote/bluetooth_constants.dart';
 import 'package:smart_handagote/constant.dart';
+import 'package:smart_handagote/logic/firebase_helper.dart';
+import 'package:collection/collection.dart';
 
 class AddFingerprintPage extends StatefulWidget {
   const AddFingerprintPage({super.key});
@@ -9,36 +14,225 @@ class AddFingerprintPage extends StatefulWidget {
 }
 
 class _AddFingerprintPageState extends State<AddFingerprintPage> {
+  FlutterBluePlus flutterBluePlus = FlutterBluePlus.instance;
+  List<BluetoothDevice> devices = [];
+  final TextEditingController controller = TextEditingController();
+  BluetoothDevice? device;
+  String _name = '';
+  String _uid = '';
+
+  bool isLoading = false; // ロード中かどうかを表す状態変数
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('指紋登録'),
-        backgroundColor: Constant.darkGray,
-        foregroundColor: Constant.white,
-      ),
-      backgroundColor: Constant.black,
-      body: _addFingerprintWidget(),
+  void initState() {
+    super.initState();
+    // Future(() async {
+    //   FirebaseHelper().getNumberOfDevices();
+    // });
+    Future(() async {
+      fetchDataFromFirebase(); // Firebaseからデータを取得する
+    });
+    startScan();
+  }
+
+// Firebaseからデータを取得する非同期メソッド
+  Future<void> fetchDataFromFirebase() async {
+    try {
+      final User? user = FirebaseAuth.instance.currentUser;
+      if (user != null) {
+        String name = await FirebaseHelper().getUserName(user.uid);
+        setState(() {
+          _uid = user.uid;
+          _name = name;
+        });
+      } else {
+        // if (!mounted) return;
+        // DialogHelper.showCustomDialog(
+        //     context: context, title: 'ログインしてください', message: '');
+      }
+    } catch (e) {
+      setState(() {
+        _uid = "error";
+        _name = "error";
+      });
+      if (!mounted) return;
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Error fetching data from Firebase.')),
+      );
+    } finally {
+      // ロードが完了したので isLoading を false に設定
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  // Bluetoothデバイスからサービスを見つけて返す関数
+  Future<BluetoothService?> findService(device) async {
+    try {
+      final List<BluetoothService> services = await device.discoverServices();
+      // Bluetoothデバイスからサービスを探索して取得
+      for (final uuid in BluetoothConstants.serviceUuids) {
+        // 指定したUUIDに一致するサービスを検索して返す
+        final matchingService = services.firstWhereOrNull(
+          (service) => service.uuid.toString() == uuid,
+        );
+
+        if (matchingService != null) {
+          return matchingService;
+        }
+      }
+
+      return null; // どのUUIDにも一致しなかった場合
+    } catch (e) {
+      // サービスが見つからない場合にエラーメッセージを出力し、nullを返す
+      print('Error finding service: $e');
+      return null;
+    }
+  }
+
+  startScan() {
+    devices = [];
+
+    // Bluetoothデバイスをスキャンする
+    flutterBluePlus.startScan(timeout: const Duration(seconds: 4));
+
+    // スキャンした結果を受け取る
+    flutterBluePlus.scanResults.listen((results) {
+      setState(() {
+        for (ScanResult result in results) {
+          if (!devices.contains(result.device)) {
+            // 対応しているserviceが含まれているのかを確認する
+            for (final uuid in BluetoothConstants.serviceUuids) {
+              final targetService =
+                  result.advertisementData.serviceUuids.contains(uuid);
+              // 対応しているserviceが含まれていれば、デバイス一覧の配列に追加する。
+              // 自分の端末のみを対象にするため。
+              if (targetService) {
+                devices.add(result.device);
+              }
+            }
+          }
+        }
+      });
+    });
+
+    // 以前に接続したことのあるBluetoothを取得する
+    flutterBluePlus.connectedDevices.then((value) {
+      setState(() {
+        for (final result in value) {
+          if (!devices.contains(result)) {
+            devices.add(result);
+          }
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    // BLEスキャンを停止し、接続を解除する
+    flutterBluePlus.stopScan();
+    if (device != null) {
+      device!.disconnect();
+    }
+    super.dispose();
+  }
+
+  // TODO: 適当にやったの。1の時は真ん中に表示されるのあんまりかも
+  Widget _addFingerprintWidget() {
+    return Column(
+      children: List.generate(devices.length, (index) {
+        final device = devices[index];
+        if (devices.length == 1) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _addBtnWidget('No.${index + 1}', () {
+                  connectToDeviceAndSendData(device);
+                }),
+              ],
+            ),
+          );
+        } else if (devices.length >= 2) {
+          if (index % 2 == 0) {
+            final nextIndex = index + 1;
+            final nextDevice =
+                devices.length > nextIndex ? devices[nextIndex] : null;
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  _addBtnWidget('No.${index + 1}', () {
+                    connectToDeviceAndSendData(device);
+                  }),
+                  if (nextDevice != null)
+                    _addBtnWidget('No.${nextIndex + 1}', () {
+                      connectToDeviceAndSendData(nextDevice);
+                    }),
+                ],
+              ),
+            );
+          }
+        }
+        return const SizedBox(height: 0);
+      }),
     );
   }
 
-  Widget _addFingerprintWidget() {
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 40),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          _addBtnWidget('No.1', () {
-            // TODO: No.1の指紋登録処理
-          }),
-          // No.2
-          _addBtnWidget('No.2', () {
-            // TODO: No.2の指紋登録処理
-          }),
-        ],
-      ),
-    );
+// デバイスに接続しデータを送信するメソッド
+  void connectToDeviceAndSendData(BluetoothDevice device) async {
+    try {
+      await device.connect();
+      await sendUidAndNameToESP32(device);
+      await device.disconnect();
+    } catch (e) {
+      // エラーハンドリング
+    }
+  }
+
+  // BluetoothDeviceにUIDと名前を送信する関数
+  Future<void> sendUidAndNameToESP32(BluetoothDevice device) async {
+    // UIDと名前をデータとして連結
+    final sendData = '$_uid:$_name';
+    // 送信用のCharacteristicを取得
+    final sendCharacteristic =
+        await getCharByUuid(BluetoothConstants.sendUidAndNameUuid, device);
+    // 送信用のCharacteristicが取得できた場合にデータを書き込む
+    if (sendCharacteristic != null) {
+      await writeToCharacteristic(sendCharacteristic, sendData);
+    } else {
+      // if (!mounted) return;
+      // DialogHelper.showCustomDialog(
+      //     context: context, title: 'エラー', message: 'Characteristicの取得に失敗しました');
+    }
+  }
+
+  // UUIDに基づいてBluetoothCharacteristicを取得する関数
+  Future<BluetoothCharacteristic?> getCharByUuid(
+      String uuid, BluetoothDevice device) async {
+    // サービスを探索して取得
+    final service = await findService(device);
+    // 指定したUUIDに一致するCharacteristicを検索して返す
+    return service?.characteristics.firstWhereOrNull((element) {
+      return element.uuid.toString() == uuid;
+    });
+  }
+
+// BluetoothCharacteristicにデータを書き込む関数
+  Future<void> writeToCharacteristic(
+      BluetoothCharacteristic characteristic, String value) async {
+    // 文字列をバイトデータに変換して書き込むためのリストを作成
+    List<int> codeUnits = [];
+    for (int i = 0; i < value.length; i++) {
+      codeUnits.add(value.codeUnitAt(i));
+    }
+
+    // バイトデータをCharacteristicに書き込む
+    await characteristic.write(codeUnits);
   }
 
   Widget _addBtnWidget(String deviceName, GestureTapCallback onPressed) {
@@ -63,6 +257,20 @@ class _AddFingerprintPageState extends State<AddFingerprintPage> {
           ],
         ),
       ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('指紋登録'),
+        backgroundColor: Constant.darkGray,
+        foregroundColor: Constant.white,
+      ),
+      backgroundColor: Constant.black,
+      body: _addFingerprintWidget(),
     );
   }
 }
